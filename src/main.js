766aa0ed41eefef4890525391f4e2d2bf5a98170
msz64 <msz64@wp.pl>
2018-05-28 22:08:02 +0200

    resizing change

--- src/main.js
+++ src/main.js
@@ -10,19 +10,29 @@ let mainWindow
 app.on('ready', _ => {
     
     mainWindow = new BrowserWindow({
-        height: 750,
-        width: 500
+        width: 500,
+        height: 750
+        
     })
-
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 
     mainWindow.on("closed", _=>{
+        
         mainWindow = null
     })
+
+    mainWindow.on("resize", () =>{
+        let size = mainWindow.getSize()
+        size[1] = size[0]*1.5
+        mainWindow.setSize(parseInt(size[0]), parseInt(size[1]))
+        
+    })
+    
 })
 
 ipc.on("result", (event, arg)=>{
     let result = calculator.calc(arg)
     // console.log(result)
     mainWindow.webContents.send("endResult", result)
+    
 })
